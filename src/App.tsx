d8eea3c3c3443362f505d5239072e5d90f5cd813
ZingLix <me@zinglix.xyz>
2019-03-21 13:01:19 +0800

    项目初始化

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import './App.css';
+import {Button} from 'antd';
+import {Route} from 'react-router-dom';
+
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <Button>test</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
