a8a772ee06f8c2ebe2c41899873d76173b4b026d
ZingLix <me@zinglix.xyz>
2019-04-01 19:54:49 +0800

    start to migrate to api

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -8,99 +8,118 @@ const { Header, Footer, Sider, Content } = Layout;
 export class MainPage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      productCategory: [],
+      productList: [
+        {
+          id: 1,
+          name: "番茄",
+          minprice: 3.3,
+          unit: "斤",
+          img: "test.png",
+          category_id: 1
+        },
+        {
+          id: 2,
+          name: "黄瓜",
+          minprice: 4.6,
+          unit: "斤",
+          img: "test.png",
+          category_id: 2
+        },
+        {
+          id: 3,
+          name: "黄瓜",
+          minprice: 4.6,
+          unit: "斤",
+          img: "test.png",
+          category_id: 3
+        },
+        {
+          id: 4,
+          name: "黄瓜",
+          minprice: 4.6,
+          unit: "斤",
+          img: "test.png",
+          category_id: 2
+        },
+        {
+          id: 5,
+          name: "黄瓜",
+          minprice: 4.6,
+          unit: "斤",
+          img: "test.png",
+          category_id: 4
+        },
+        {
+          id: 9,
+          name: "黄瓜",
+          minprice: 4.6,
+          unit: "斤",
+          img: "test.png",
+          category_id: 4
+        },
+        {
+          id: 8,
+          name: "黄瓜",
+          minprice: 4.6,
+          unit: "斤",
+          img: "test.png",
+          category_id: 4
+        },
+        {
+          id: 6,
+          name: "黄瓜",
+          minprice: 4.6,
+          unit: "斤",
+          img: "test.png",
+          category_id: 4
+        },
+        {
+          id: 7,
+          name: "黄瓜",
+          minprice: 4.6,
+          unit: "斤",
+          img: "test.png",
+          category_id: 3
+        },
+        {
+          id: 10,
+          name: "黄瓜",
+          minprice: 4.6,
+          unit: "斤",
+          img: "test.png",
+          category_id: 3
+        }
+      ],
+      currentCategory: 0
+    };
   }
 
-  state = {
-    productCategory: [
-      { id: 0, typename: "全部" },
-      { id: 1, typename: "新鲜水果" },
-      { id: 2, typename: "时令蔬菜" },
-      { id: 3, typename: "海鲜水产" },
-      { id: 4, typename: "肉禽蛋品" }
-    ],
-    productList: [
-      {
-        id: 1,
-        name: "番茄",
-        minprice: 3.3,
-        unit: "斤",
-        img: "test.png",
-        category_id: 1
-      },
-      {
-        id: 2,
-        name: "黄瓜",
-        minprice: 4.6,
-        unit: "斤",
-        img: "test.png",
-        category_id: 2
-      },
-      {
-        id: 3,
-        name: "黄瓜",
-        minprice: 4.6,
-        unit: "斤",
-        img: "test.png",
-        category_id: 3
-      },
-      {
-        id: 4,
-        name: "黄瓜",
-        minprice: 4.6,
-        unit: "斤",
-        img: "test.png",
-        category_id: 2
-      },
-      {
-        id: 5,
-        name: "黄瓜",
-        minprice: 4.6,
-        unit: "斤",
-        img: "test.png",
-        category_id: 4
-      },
-      {
-        id: 9,
-        name: "黄瓜",
-        minprice: 4.6,
-        unit: "斤",
-        img: "test.png",
-        category_id: 4
-      },
-      {
-        id: 8,
-        name: "黄瓜",
-        minprice: 4.6,
-        unit: "斤",
-        img: "test.png",
-        category_id: 4
-      },
-      {
-        id: 6,
-        name: "黄瓜",
-        minprice: 4.6,
-        unit: "斤",
-        img: "test.png",
-        category_id: 4
-      },
-      {
-        id: 7,
-        name: "黄瓜",
-        minprice: 4.6,
-        unit: "斤",
-        img: "test.png",
-        category_id: 3
-      },
-      {
-        id: 10,
-        name: "黄瓜",
-        minprice: 4.6,
-        unit: "斤",
-        img: "test.png",
-        category_id: 3
-      }
-    ],
-    currentCategory: 0
+  componentWillMount() {
+    fetch("http://127.0.0.1:8080/api/products/category")
+      .then((response: any) => response.json())
+      .then((d: any) => {
+        this.setState({
+          productCategory: d
+        });
+      });
+  }
+
+  state: {
+    productCategory: {
+      id: number;
+      typename: string;
+    }[];
+    productList: {
+      id: number;
+      name: string;
+      minprice: number;
+      unit: string;
+      img: string;
+      category_id: number;
+    }[];
+    currentCategory: number;
   };
 
   public render() {
@@ -114,6 +133,14 @@ export class MainPage extends React.Component {
               defaultOpenKeys={["sub1"]}
               style={{ height: "100%" }}
             >
+              <Menu.Item
+                key={0}
+                onClick={() => {
+                  this.setState({ currentCategory: 0 });
+                }}
+              >
+                全部
+              </Menu.Item>
               {this.state.productCategory.map(c => (
                 <Menu.Item
                   key={c.id}
